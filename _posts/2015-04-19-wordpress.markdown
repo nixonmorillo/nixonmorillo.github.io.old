ece12444e69e0f0dd5def0b48a2e5785acdb8f0e
Nixon Morillo <nixonmorillo@gmail.com>
2015-08-12 04:23:37 -0430

    Pagina Nueva

---
+++ _posts/2015-04-19-wordpress.markdown
@@ -0,0 +1,11 @@
+---
+layout: default
+modal-id: 1
+date: 2015-04-19
+img: wordpress.png
+alt: image-alt
+project-date: November 2014
+client: Encom de Venezuela C.A.
+category: Web Development
+description: Make a website for a electronic's solutions enterprice <a href="http://www.encomdevenezuela.com/">Encom de Venezuela</a>. The portal was made in Wordpress, with responsive desing and Search engine optimization(SEO). 
+---
---
+++ js/contact_me.js
@@ -0,0 +1,70 @@
+$(function() {
+
+    $("input,textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var phone = $("input#phone").val();
+            var message = $("textarea#message").val();
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+            $.ajax({
+                url: "//formspree.io/contacto@nixon.xyz",
+                type: "POST",
+                data: {
+                    nombre: name,
+                    telefono: phone,
+                    email: email,
+                    mensaje: message
+                },
+                cache: false,
+                success: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been send!. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function() {
+                    // Success message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Im Sorry " + firstName + ", the server is not responding. Try again, or send your info to me@nixon.xyz!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+            })
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
